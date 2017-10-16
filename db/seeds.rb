5464557970f7a8ad08b2aeea196032740b417cbd
Alex Cipriano <aciprian@uncc.edu>
2017-10-16 17:36:05 +0000

    Done with section 4.4, and fixed seeding the website

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # Seed the RottenPotatoes DB with some movies.
+
 more_movies = [
   {:title => 'Aladdin', :rating => 'G',
     :release_date => '25-Nov-1992'},
@@ -9,10 +10,7 @@ more_movies = [
   {:title => 'Raiders of the Lost Ark', :rating => 'PG',
     :release_date => '12-Jun-1981'}
 ]
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
